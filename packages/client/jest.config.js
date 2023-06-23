c3680cdd865122208ac2f5639bbdf7ec1637b4f2
Денис Булгаков <bulgakov_denis@Noutbuk-Denis.local>
2023-06-23 14:07:41 +0300

    feat: добавлен конфиг для jest

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -5,6 +5,18 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  watchPathIgnorePatterns: ['node_modules'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/api/(.*)$': '<rootDir>/api/$1',
+    '^@/app/(.*)$': '<rootDir>/app/$1',
+    '^@/helpers/(.*)$': '<rootDir>/helpers/$1',
+    '^@/hooks/(.*)$': '<rootDir>/hooks/$1',
+    '^@/modules/(.*)$': '<rootDir>/modules/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^@/routes/(.*)$': '<rootDir>/routes/$1',
+    '^@/ui/(.*)$': '<rootDir>/ui/$1'
+  },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
