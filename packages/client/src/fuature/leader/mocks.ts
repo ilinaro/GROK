e2a56cddbe180700957773990844219dd307b3ae
ilya.kaverin <ilya.kaverin@gazprombank.ru>
2023-06-26 13:58:03 +0300

    feat: Добавлен блок лидеров

---
+++ packages/client/src/fuature/leader/mocks.ts
@@ -0,0 +1,5 @@
+export const leadersMock = [
+  { avatar: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg', username: 'Mortymerr', points: 1000 },
+  { avatar: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg', username: 'Rick', points: 999 },
+  { avatar: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg', username: 'Jerry', points: 998 },
+];
