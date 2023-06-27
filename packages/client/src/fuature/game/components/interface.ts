bd76e2f270857e04242eeff6a854dd5cd206e537
Oleg Ilin <ilindev78@gmal.com>
2023-06-28 01:54:49 +0300

    upd добавил окно Canvas и добавил квадрат, задал цвет, разбил все на части

---
+++ packages/client/src/fuature/game/components/interface.ts
@@ -0,0 +1,11 @@
+export enum Color {
+  VIOLET = '#6e3dc8',
+  VIOLET_DARKEN = '#5830a5',
+  WHITE_DARKEN = '#8b97ae',
+  WHITE = '#ffffff',
+  BLUE = '#6781f7',
+  PINK = '#d752aa',
+  YELLOW = '#fcba06',
+  BLACK = '#192233',
+  BLACK_LIGHT = '#212c42',
+}
