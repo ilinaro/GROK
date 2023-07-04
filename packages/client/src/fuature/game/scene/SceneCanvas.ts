18166a4559f8ed28fc5fd4f0e586bbb445f38fd7
Oleg Ilin <ilindev78@gmal.com>
2023-07-04 04:47:59 +0300

    добавлена игровая механика

--- packages/client/src/fuature/game/scene/SceneCanvas.ts
+++ packages/client/src/fuature/game/scene/SceneCanvas.ts
@@ -1,50 +1,104 @@
-import { useEffect, useRef } from 'react';
+import Player from './Player';
 
-import ballImage from '../source/ball.png';
-import { drawBall } from '../figure';
-
-export const useSceneCanvas = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas?.getContext('2d');
+export class SceneCanvas {
+  init(canvas: HTMLCanvasElement | null, ball: HTMLImageElement) {
+    if (!canvas) return;
+    const context = canvas.getContext('2d');
+    const canvasWidth = canvas.width || 0;
+    const canvasHeight = canvas.height || 0;
 
     const resizeCanvas = () => {
-      if (canvas) {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        document.body.style.overflow = 'hidden';
-      }
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      document.body.style.overflow = 'hidden';
     };
 
-    const ball = new Image();
-    ball.src = ballImage;
-
-    const draw = () => {
-      if (context) {
-        const canvasWidth = canvas?.width || 0;
-        const canvasHeight = canvas?.height || 0;
-
-        drawBall({ ball, context, canvasWidth, canvasHeight });
-      }
+    const keys = {
+      w: {
+        pressed: false,
+      },
+      a: {
+        pressed: false,
+      },
+      d: {
+        pressed: false,
+      },
     };
 
     resizeCanvas();
-    ball.onload = draw;
+    const player = new Player(context, ball, canvasWidth, canvasHeight);
 
+    function animate() {
+      if (context) {
+        window.requestAnimationFrame(animate);
+        context.clearRect(0, 0, canvasWidth, canvasHeight);
+        if (keys.d.pressed) {
+          player.velocity.x = 4;
+        } else if (keys.a.pressed) {
+          player.velocity.x = -4;
+        }
+        player.draw();
+        player.update();
+      }
+    }
+
+    animate();
+
+    window.addEventListener('keydown', (event) => {
+      switch (event.key) {
+        // up
+        case ' ':
+          if (player.velocity.y === 0) player.velocity.y = -20;
+          break;
+        case 'w':
+          if (player.velocity.y === 0) player.velocity.y = -20;
+          break;
+        case 'ArrowUp':
+          if (player.velocity.y === 0) player.velocity.y = -20;
+          break;
+        // left
+        case 'a':
+          keys.a.pressed = true;
+          break;
+        case 'ArrowLeft':
+          keys.a.pressed = true;
+          break;
+        // right
+        case 'd':
+          keys.d.pressed = true;
+          break;
+        case 'ArrowRight':
+          keys.d.pressed = true;
+          break;
+      }
+    });
+
+    window.addEventListener('keyup', (event) => {
+      switch (event.key) {
+        // left
+        case 'a':
+          keys.a.pressed = false;
+          break;
+        case 'ArrowLeft':
+          keys.a.pressed = false;
+          break;
+        // right
+        case 'd':
+          keys.d.pressed = false;
+          break;
+        case 'ArrowRight':
+          keys.d.pressed = false;
+          break;
+      }
+    });
     window.addEventListener('resize', () => {
       resizeCanvas();
-      draw();
     });
 
     return () => {
       window.removeEventListener('resize', () => {
         resizeCanvas();
-        draw();
       });
     };
-  }, []);
-
-  return canvasRef;
-};
+  }
+}
---
+++ packages/client/src/fuature/game/scene/WindowCanvas.ts
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'react';
+
+import { SceneCanvas } from './SceneCanvas';
+import ballImage from '../source/ball.png';
+
+export const WindowCanvas = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ball = new Image();
+    ball.src = ballImage;
+
+    const sceneCanvas = new SceneCanvas();
+    const cleanup = sceneCanvas.init(canvas, ball);
+
+    return cleanup;
+  }, []);
+
+  return canvasRef;
+};
