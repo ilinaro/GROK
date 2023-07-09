6f95b0ca9e6622ccf9369e2ef3ddee3a40b3be85
ilya.kaverin <ilya.kaverin@gazprombank.ru>
2023-07-09 22:53:36 +0300

    feat: добавлено апи смены пароля

--- packages/client/src/store/thunks/change-user-data.ts
+++ packages/client/src/store/thunks/change-user-data.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { user } from 'config/users.config';
+import { navigate } from 'react-router-dom';
 
 export const setAvatar = async (data: any) => {
   try {
@@ -18,5 +19,24 @@ export const changePassword = async (data: any) => {
     if (error instanceof Error) {
       console.log(error);
     }
+  }
+};
+
+export const changePassword = (data: any) => async (dispatch: AppDispatch) => {
+  dispatch(setUserLoadingAC(true));
+  try {
+    const response = await axios.put(user.changePassword, data, { withCredentials: true });
+    if (response.status === 200) {
+      dispatch(setUserAuthAC());
+      const { data } = await getUser();
+
+      dispatch(setUserAC(data));
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      dispatch(setUserErrorAC(error.message));
+    }
+  } finally {
+    dispatch(setUserLoadingAC(false));
   }
 };
