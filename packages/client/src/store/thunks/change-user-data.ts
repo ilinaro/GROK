fcb632795479ce4dd8c1072095f873d5aba17e8a
ilya.kaverin <ilya.kaverin@gazprombank.ru>
2023-07-09 22:53:36 +0300

    feat: добавлено апи смены пароля

---
+++ packages/client/src/store/thunks/change-user-data.ts
@@ -0,0 +1,47 @@
+import { setUserAC, setUserAuthAC, setUserErrorAC, setUserLoadingAC } from '@store/actions/userAction';
+import { AppDispatch } from '@store/types';
+import { User } from '@store/types/userTypes';
+import axios from 'axios';
+import { auth } from 'config/apiRoutes.config';
+import { user } from 'config/users.config';
+import { navigate } from 'react-router-dom';
+
+export const getUser = async () => await axios.get<User>(auth.user, { withCredentials: true });
+
+export const setAvatar = (data: any) => async (dispatch: AppDispatch) => {
+  dispatch(setUserLoadingAC(true));
+  try {
+    const response = await axios.put(user.setAvatar, data, { withCredentials: true });
+    if (response.status === 200) {
+      dispatch(setUserAuthAC());
+      const { data } = await getUser();
+
+      dispatch(setUserAC(data));
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      dispatch(setUserErrorAC(error.message));
+    }
+  } finally {
+    dispatch(setUserLoadingAC(false));
+  }
+};
+
+export const changePassword = (data: any) => async (dispatch: AppDispatch) => {
+  dispatch(setUserLoadingAC(true));
+  try {
+    const response = await axios.put(user.changePassword, data, { withCredentials: true });
+    if (response.status === 200) {
+      dispatch(setUserAuthAC());
+      const { data } = await getUser();
+
+      dispatch(setUserAC(data));
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      dispatch(setUserErrorAC(error.message));
+    }
+  } finally {
+    dispatch(setUserLoadingAC(false));
+  }
+};
