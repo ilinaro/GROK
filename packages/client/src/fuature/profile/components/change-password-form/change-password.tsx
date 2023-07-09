f5db33bdccac079bba1a2a8b479e58d7ccbd3d7d
ilya.kaverin <ilya.kaverin@gazprombank.ru>
2023-07-09 22:53:36 +0300

    feat: добавлено апи смены пароля

--- packages/client/src/fuature/profile/components/change-password-form/change-password.tsx
+++ packages/client/src/fuature/profile/components/change-password-form/change-password.tsx
@@ -1,36 +1,105 @@
 import React, { useState } from 'react';
-import { Input } from '../input';
 import { useForm } from 'react-hook-form';
 import styles from './styles.module.scss';
 import { Button } from '@components/design-system';
+import { FormInput } from '@components/specific/FormInput/FormInput';
+import { HidePassSVG } from '@components/design-system/SVG/HidePassSVG';
+import { ShowPassSVG } from '@components/design-system/SVG/ShowPassSVG';
+import { PASSWORD_REGEX } from 'fuature/profile/constants';
+import { changePassword } from '@store/thunks/change-user-data';
 
 interface IChangePasswordForm {
   setMode(value: string): void;
 }
 
-export const ChangePasswordForm: React.FC<IChangePasswordForm> = ({ setMode }) => {
-  const [isEdit, setIsEdit] = useState(false);
+interface IShowPass {
+  old_password: boolean;
+  new_password: boolean;
+  confirmPassword: boolean;
+}
 
+export const ChangePasswordForm: React.FC<IChangePasswordForm> = ({ setMode }) => {
+  const [isPasswordShow, setIsPasswordShow] = useState<IShowPass>({
+    old_password: false,
+    new_password: false,
+    confirmPassword: false,
+  });
   const { control, watch, handleSubmit, formState } = useForm<any>({
     defaultValues: {
       old_password: '',
       new_password: '',
-      confirm_password: '',
+      confirmPassword: '',
     },
   });
 
+  const onSubmit = (data: IShowPass) => {
+    const { old_password, new_password } = data;
+
+    changePassword({ oldPassword: old_password, newPassword: new_password });
+  };
+
+  const toggleShowPassword = (key: string) =>
+    setIsPasswordShow((prev) => {
+      const value = prev[key];
+
+      console.log({ ...prev, [key]: !value });
+
+      return { ...prev, [key]: !value };
+    });
+
+  const showOrHidddenIcon = (key: string) => {
+    if (!watch().old_password.length) return;
+
+    if (isPasswordShow[key]) {
+      return <HidePassSVG onClick={() => toggleShowPassword(key)} />;
+    } else {
+      return <ShowPassSVG onClick={() => toggleShowPassword(key)} />;
+    }
+  };
+
+  const validatePasswordMatch = (value: string) => {
+    const { new_password } = watch();
+    return value === new_password || 'Пароли не совпадают';
+  };
+
   return (
     <form className={styles.FormContainer}>
-      <Input type="text" placeholder="Старый пароль" control={control} name="old_password" label="Старый пароль" />
-      <Input type="text" placeholder="Новый пароль" control={control} name="new_password" label="Новый пароль" />
-      <Input
-        type="text"
-        placeholder="Подтвердите пароль"
+      <FormInput
+        name="old_password"
+        label="Cтарый пароль"
+        type={isPasswordShow.old_password ? 'text' : 'password'}
         control={control}
-        name="confirm_password"
-        label="Подтвердите пароль"
+        rules={{
+          required: 'Это поле обязательно',
+        }}
+        rightAddon={showOrHidddenIcon('old_password')}
       />
-      <Button>Сохранить</Button>
+      <FormInput
+        name="new_password"
+        label="Новый пароль"
+        type={isPasswordShow.new_password ? 'text' : 'password'}
+        control={control}
+        rules={{
+          required: 'Это поле обязательно',
+          pattern: {
+            value: PASSWORD_REGEX,
+            message: 'min 8 символов, min 1 цифра и 1 загл. буква',
+          },
+        }}
+        rightAddon={showOrHidddenIcon('new_password')}
+      />
+      <FormInput
+        name="confirmPassword"
+        label="Повторите пароль"
+        type={isPasswordShow.confirmPassword ? 'text' : 'password'}
+        control={control}
+        rules={{
+          validate: validatePasswordMatch,
+          required: 'Это поле обязательно',
+        }}
+        rightAddon={showOrHidddenIcon('confirmPassword')}
+      />
+      <Button onClick={handleSubmit(onSubmit)}>Сохранить</Button>
       <Button onClick={() => setMode('profile')}>Назад</Button>
     </form>
   );
--- packages/client/src/store/thunks/change-user-data.ts
+++ packages/client/src/store/thunks/change-user-data.ts
@@ -4,6 +4,7 @@ import { User } from '@store/types/userTypes';
 import axios from 'axios';
 import { auth } from 'config/apiRoutes.config';
 import { user } from 'config/users.config';
+import { navigate } from 'react-router-dom';
 
 export const getUser = async () => await axios.get<User>(auth.user, { withCredentials: true });
 
@@ -11,6 +12,25 @@ export const setAvatar = (data: any) => async (dispatch: AppDispatch) => {
   dispatch(setUserLoadingAC(true));
   try {
     const response = await axios.put(user.setAvatar, data, { withCredentials: true });
+    if (response.status === 200) {
+      dispatch(setUserAuthAC());
+      const { data } = await getUser();
+
+      dispatch(setUserAC(data));
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      dispatch(setUserErrorAC(error.message));
+    }
+  } finally {
+    dispatch(setUserLoadingAC(false));
+  }
+};
+
+export const changePassword = (data: any) => async (dispatch: AppDispatch) => {
+  dispatch(setUserLoadingAC(true));
+  try {
+    const response = await axios.put(user.changePassword, data, { withCredentials: true });
     if (response.status === 200) {
       dispatch(setUserAuthAC());
       const { data } = await getUser();
