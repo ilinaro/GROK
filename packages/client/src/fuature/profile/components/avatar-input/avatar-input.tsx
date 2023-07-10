204bdf06c3650eb5960ac2a3b278b37b710f004a
ilya.kaverin <ilya.kaverin@gazprombank.ru>
2023-07-10 11:08:33 +0300

    fix: мелкие исправления

---
+++ packages/client/src/fuature/profile/components/avatar-input/avatar-input.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { AvatarSVG } from '@components/design-system';
+import { RegisterOptions, useController } from 'react-hook-form';
+import styles from './style.module.scss';
+import { setAvatar } from '@store/thunks/change-user-data';
+
+interface IAvatarInput {
+  name: string;
+  rules: RegisterOptions;
+  control: any;
+}
+
+export const AvatarInput: React.FC<IAvatarInput> = ({ name, control, rules }) => {
+  const {
+    field: { ref, value, ...rest },
+    fieldState,
+  } = useController({
+    name: name,
+    control,
+    rules: rules,
+  });
+
+  useEffect(() => {
+    setAvatar(value);
+  }, [value]);
+
+  return (
+    <div className={styles.Avatar}>
+      <label htmlFor={name}>
+        <input type="file" ref={ref} id={name} {...rest} />
+        {value ? <img src={value} alt="avatar" /> : <AvatarSVG width={64} />}
+      </label>
+    </div>
+  );
+};
