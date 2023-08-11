3058f1a34055f6d0e05c17e5aa8373269f65156f
PtitsynAA <ptitsynaa@alfastrah.ru>
2023-08-11 09:54:21 +0300

    последние изменения

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,67 @@
 import dotenv from 'dotenv'
 import cors from 'cors'
+import * as fs from 'fs'
+import * as path from 'path'
+// import * as vite from '@vitejs/plugin-react'
+import { createServer as createViteServer } from 'vite';
 dotenv.config()
 
 import express from 'express'
-import { createClientAndConnect } from './db'
 
-const app = express()
-app.use(cors())
-const port = Number(process.env.SERVER_PORT) || 3001
+// import { createClientAndConnect } from './db'
 
-createClientAndConnect()
+async function startServer() {
+  const app = express()
+  app.use(cors())
+  const port = Number(process.env.SERVER_PORT) || 3001
 
-app.get('/', (_, res) => {
-  res.json('👋 Howdy from the server :)')
-})
+  // createClientAndConnect()
 
-app.listen(port, () => {
-  console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
-})
+  const vite = await createViteServer({
+    server: { middlewareMode: true },
+    appType: 'custom'
+  });
+
+  app.use(vite.middlewares);
+
+  app.get('/api', (_, res) => {
+    res.json('👋 Howdy from the server :)')
+  })
+
+  // const distPath = path.dirname(require.resolve('../client/dist/index.html'));
+  const distPath = path.dirname(require.resolve('client/dist/index.html'));
+  // const ssrClientPath = path.dirname(require.resolve('../client/ssr-dist/client.cjs'));
+  const ssrClientPath = path.dirname(require.resolve('client/ssr-dist/ssr.cjs'));
+
+  app.use('/assets', express.static(path.resolve(distPath, 'assets')));
+
+  app.use('*', async (req, res, next) => {
+    const url = req.originalUrl
+
+    try {
+      let template = fs.readFileSync(
+        path.resolve(distPath, 'index.html'),
+        'utf-8',
+      )
+
+      const { render } = await import(ssrClientPath);
+
+      const appHtml = await render(url)
+
+      const html = template.replace(`<!--ssr-outlet-->`, appHtml)
+
+      res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+    } catch (e) {
+      // If an error is caught, let Vite fix the stack trace so it maps back
+      // to your actual source code.
+      // vite.ssrFixStacktrace(e)
+      next(e)
+    }
+  })
+
+  app.listen(port, () => {
+    console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
+  })
+}
+
+startServer();
